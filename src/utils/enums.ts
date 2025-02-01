c67e50eaaeed3a88a58e253bead022a0845d2cc9
rameshwar mane <manerameshwar909@gmail.com>
2025-02-01 22:03:42 +0530

    Initial commit

---
+++ src/utils/enums.ts
@@ -0,0 +1,33 @@
+export enum TypeOfList {
+  Today,
+  All,
+  ByDays,
+  Dashboard,
+}
+
+export enum SortingBy {
+  UsageTime,
+  Sessions,
+}
+
+export enum SettingsTab {
+  Dashboard,
+  WebsiteStats,
+  GeneralSettings,
+  WhiteList,
+  Limits,
+  Notifications,
+  Pomodoro,
+  About,
+  Donate,
+}
+
+export enum ListWithTime {
+  Limits,
+  Notifications,
+}
+
+export enum TypeOfUrl {
+  WebSite,
+  Document,
+}
